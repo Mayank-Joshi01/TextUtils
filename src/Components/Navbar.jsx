766aa17d7d9982eab1c331d09c77a65f391b7fe5
Mayank-Joshi01 <mayankjoshi.in.123@gmail.com>
2025-05-07 21:03:56 +0530

    Added a whitch to toogle theme

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,13 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { NavLink } from "react-router-dom";
 export default function Navbar(props){
+
+   /// To update Theme
+  const updateThem = (e) => {console.log(e.target.checked)
+    if(e.target.checked){
+      props.TMD()
+  }
+  else{
+      props.TML()}
+  }
+
     return(<>
      <h1 className={`text-center bg-light my-0 ${(props.Mode==='Light')?'':'bg-black text-light'}`}>{props.title}</h1>
-<nav className={`navbar navbar-expand-lg ${(props.Mode==='Light')?'':'bg-black text-light'} bg-light `}>
+<nav className={`navbar navbar-expand-lg ${(props.Mode==='Light')?'':'bg-black text-light'} bg-light position-relative`}>
+
+{/* Switch to toogle theam */ }
+<span className='position-absolute end-0 top-3 px-2'>
+  <span className="form-check form-switch">
+<input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={(e)=>{updateThem(e)}}/>
+</span>
+
+    </span>
   <div className="container-fluid">
-  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button className={`navbar-toggler mb-1 ${(props.Mode==='Light')?'':'bg-light'}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+   
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav">
         <li className="nav-item text-center" >
@@ -17,23 +36,11 @@ export default function Navbar(props){
         <li className="nav-item text-center">
           <NavLink className={(e)=>{return e.isActive?"nav-link active btn btn-outline-success text-light":"nav-link active"}} style={{color: props.Mode==='Light'?"black":"white"}} aria-current="page" to="/Textutils/About" end>About</NavLink>
         </li>
-        <li className="nav-item dropdown">
-          <button className=" w-100 active nav-link dropdown-toggle" style={{color: props.Mode==='Light'?"black":"white"}} data-bs-toggle="dropdown" aria-expanded="false">
-            Theme
-          </button>
-          <ul className="dropdown-menu" style={{padding:0}} >
-            <li><button className="dropdown-item text-center" onClick={props.TMD} style={{color: props.Mode==='Light'?"black":"white" , backgroundColor : props.Mode==='Light'?"rgb(245 245 245)":"black" , border:props.Mode==='Light'?'':'1px solid white'}} >Dark</button></li>
-            <li><button className="dropdown-item text-center" onClick={props.TML}  style={{color: props.Mode==='Light'?"black":"white" , backgroundColor : props.Mode==='Light'?"rgb(245 245 245)":"black" , border:props.Mode==='Light'?'':'1px solid white'}}>Light</button></li>
-          </ul>
-        </li>
       </ul>
     </div>
   </div>
 </nav>
-
-
-
-    </>
+ </>
     )
 }
 
